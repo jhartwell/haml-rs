db839d79878a954faf0317045ea3e4d53203fe8e
Jon Hartwell <jon@dontbreakthebuild.com>
2018-07-26 00:33:11 -0500

    Removing unnecessary code that isn't used

--- src/values.rs
+++ src/values.rs
@@ -16,27 +16,6 @@ pub enum Token {
     DocType(),
 }
 
-impl ToString for Token {
-    fn to_string(&self) -> String {
-        match self {
-            Token::ForwardSlash() => "/".to_string(),
-            Token::Period() => ".".to_string(),
-            Token::Equal() => "=".to_string(),
-            Token::DoubleQuote() => "\"".to_string(),
-            Token::OpenParen() => "(".to_string(),
-            Token::CloseParen() => ")".to_string(),
-            Token::Whitespace() => " ".to_string(),
-            Token::PercentSign() => "%".to_string(),
-            Token::AtSymbol() => "@".to_string(),
-            Token::Hashtag() => "#".to_string(),
-            Token::Text(text) => text.to_string(),
-            Token::EndLine() => "\n".to_string(),
-            Token::Indentation(_u) => "  ".to_string(),
-            Token::DocType() => "!!!".to_string(),
-        }
-    }
-}
-
 impl Token {
     pub fn is_delim(ch: &char) -> bool {
         match ch {
